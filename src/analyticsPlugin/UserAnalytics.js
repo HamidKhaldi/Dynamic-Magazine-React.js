cf64d8f6fdb5159ba4a223cf2af65e160a589d1e
HamidKhaldi <85360532+HamidKhaldi@users.noreply.github.com>
2024-01-13 14:41:05 +0000

    Update UserAnalytics.js

--- src/analyticsPlugin/UserAnalytics.js
+++ src/analyticsPlugin/UserAnalytics.js
@@ -120,66 +120,6 @@ const UserAnalytics = ({ children }) => {
         }
       });
     }
-  
-
-    // axios
-    //   .get(
-    //     "https://eygb.sharepoint.com/sites/Creative-UK-Analytics/FSO-Together/_api/web/lists/GetByTitle('Lst_User-Analytics')/items?select=*&top=5000",
-    //     {
-    //       headers: {
-    //         "Access-Control-Allow-Origin": "*",
-    //         "Content-Type": "application/json",
-    //       },
-    //     }
-    //   )
-    //   .then((response) => {
-    //     //console.log('userAnalytics response', response.data.value);
-    //     setUserAnalytics(response.data.value);
-    //     //console.log('userAnalytics', userAnalytics);
-    //     let titleArr = [],
-    //       deptArr = [],
-    //       locationArr = [],
-    //       titleCountObj = {},
-    //       deptCountObj = {},
-    //       locationCountObj = {};
-
-    //     setUserData(true);
-    //     userAnalytics.map((user) => {
-    //       titleArr.push(user.Title);
-    //       deptArr.push(user.Department);
-    //       locationArr.push(user.Location);
-    //     });
-
-    //     titleArr.map((a) => {
-    //       a = addQuotes(a);
-    //       if (a in titleCountObj) titleCountObj[a]++;
-    //       else titleCountObj[a] = 1;
-    //     });
-
-    //     setTitleObj(titleCountObj);
-    //     //console.log('titleObj ', titleCountObj);
-
-    //     deptArr.map((a) => {
-    //       a = addQuotes(a);
-    //       if (a in deptCountObj) deptCountObj[a]++;
-    //       else deptCountObj[a] = 1;
-    //     });
-
-    //     setDeptObj(deptCountObj);
-    //     //console.log('deptObj ', deptCountObj);
-
-    //     locationArr.map((a) => {
-    //       a = addQuotes(a);
-    //       if (a in locationCountObj) locationCountObj[a]++;
-    //       else locationCountObj[a] = 1;
-    //     });
-
-    //     setLocationObj(locationCountObj);
-    //     //console.log('locationObj ', locationCountObj);
-    //   })
-    //   .catch(function (error) {
-    //     //console.log('analytics list error', error);
-    //   });
   }, []);
 
   return <>{children}</>;
