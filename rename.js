bffda2331232f391c760e183e7097f147b004fca
HamidKhaldi <85360532+HamidKhaldi@users.noreply.github.com>
2024-01-13 14:49:49 +0000

    Update rename.js

--- rename.js
+++ rename.js
@@ -28,12 +28,8 @@ switch (environment) {
 var content = fs.readFileSync(input, "utf8");
 // console.log(content);
 // take test site of of all urls
-// https://eygb.sharepoint.com/sites/diversityequityandinclusivenessuk/SiteAssets/Pages/static
-
-// 2) partial called 'sharepoint.css' including all overrides / hide for sharepoint csss....
 
 var newStr = content.replace(/static/g, pathString);
-// var newStr = content.replace(/static/g, 'https://eygb.sharepoint.com/sites/diversityequityandinclusivenessuk/SiteAssets/test-site/Pages/static');
 // console.log(newStr);
 
 newStr = newStr.replace(/\/https/g, "https");
